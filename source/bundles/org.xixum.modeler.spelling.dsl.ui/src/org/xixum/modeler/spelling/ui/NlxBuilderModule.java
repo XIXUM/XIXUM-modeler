381239b32dc8f34ed0ff89ab11971ce1ede2e52b
Felix Schaller <schaller@validas.de>
2025-03-28 12:12:50 +0100

    Add new language packages

---
+++ source/bundles/org.xixum.modeler.spelling.dsl.ui/src/org/xixum/modeler/spelling/ui/NlxBuilderModule.java
@@ -0,0 +1,21 @@
+package org.xixum.modeler.spelling.ui;
+
+import com.google.inject.AbstractModule;
+
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.xtext.builder.debug.IBuildLogger;
+import org.xixum.modeler.spelling.ui.editor.NlxBuildConsole;
+
+@SuppressWarnings("all")
+public class NlxBuilderModule extends AbstractModule {
+  public NlxBuilderModule() {
+  }
+  
+  @Override
+  public void configure() {
+    this.<IExtensionRegistry>bind(IExtensionRegistry.class).toInstance(Platform.getExtensionRegistry());
+    this.<IExtensionRegistry>bind(IExtensionRegistry.class).toInstance(Platform.getExtensionRegistry());
+    this.<IBuildLogger>bind(IBuildLogger.class).to(NlxBuildConsole.Logger.class);
+  }
+}
